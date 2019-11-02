80ce3c7a82d395ae71891c45511f9810ff30232a
harcipulyka <55332570+harcipulyka@users.noreply.github.com>
2019-11-02 15:32:45 +0100

    Add files via upload

---
+++ DinoBot/DinoBot/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace DinoBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ss = new System.Windows.Forms.PictureBox();
+            this.start = new System.Windows.Forms.Button();
+            this.DelayScreen = new System.Windows.Forms.Label();
+            this.stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ss)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ss
+            // 
+            this.ss.Location = new System.Drawing.Point(12, 12);
+            this.ss.Name = "ss";
+            this.ss.Size = new System.Drawing.Size(550, 125);
+            this.ss.TabIndex = 0;
+            this.ss.TabStop = false;
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.Black;
+            this.start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.start.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.start.ForeColor = System.Drawing.Color.IndianRed;
+            this.start.Location = new System.Drawing.Point(571, 12);
+            this.start.Margin = new System.Windows.Forms.Padding(0);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(86, 42);
+            this.start.TabIndex = 4;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // DelayScreen
+            // 
+            this.DelayScreen.AutoSize = true;
+            this.DelayScreen.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DelayScreen.Location = new System.Drawing.Point(572, 98);
+            this.DelayScreen.Name = "DelayScreen";
+            this.DelayScreen.Size = new System.Drawing.Size(52, 18);
+            this.DelayScreen.TabIndex = 6;
+            this.DelayScreen.Text = "Delay";
+            this.DelayScreen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.Black;
+            this.stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stop.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.stop.ForeColor = System.Drawing.Color.IndianRed;
+            this.stop.Location = new System.Drawing.Point(571, 54);
+            this.stop.Margin = new System.Windows.Forms.Padding(0);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(86, 42);
+            this.stop.TabIndex = 7;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = false;
+            this.stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(669, 150);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.DelayScreen);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.ss);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DinoBot";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ss)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ss;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label DelayScreen;
+        private System.Windows.Forms.Button stop;
+    }
+}
+
---
+++ DinoBot/DinoBot/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace DinoBot
+{
+    public partial class Form1 : Form
+    {
+        #region publics
+
+        public static Rectangle rectangle = new Rectangle(675, 195, 550, 125);
+
+        InputSimulator inputSimulator = new InputSimulator();
+
+        public Color backgroundColor;
+        public Color obstacleColor;
+        Thread T;
+
+        public int delay = 0;
+        public int loopCounter = 0;
+        public bool isActive = true;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //T = new Thread(Loop);
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            //T.Start();
+            Loop();
+        }
+
+        public void ColorUpdate(Bitmap bitmap)
+        {
+            obstacleColor = bitmap.GetPixel(20, 40);
+            /*
+            akadalySample.BackColor = akadaly;
+            akadalySample.Refresh();
+            */
+
+            backgroundColor = bitmap.GetPixel(6, 110);
+            /*
+            hatterSample.BackColor = hatter;
+            hatterSample.Refresh();
+            */
+        }
+
+        public void Loop()
+        {
+            while (true)
+            {
+                Bitmap bitmap = new Bitmap(rectangle.Width, rectangle.Height, PixelFormat.Format32bppArgb);
+                Graphics graphics = Graphics.FromImage(bitmap);
+                graphics.CopyFromScreen(rectangle.Left, rectangle.Top, 0, 0, bitmap.Size, CopyPixelOperation.SourceCopy);
+
+                ColorUpdate(bitmap);
+
+                WhatToDo(bitmap);
+
+                Updater(bitmap);
+            }
+        }
+
+        private void WhatToDo(Bitmap bitmap)
+        {
+            CheckJumpHeight(bitmap);
+            CheckDuckHeight(bitmap);
+            DelayUpdater();
+        }
+
+        private void CheckJumpHeight(Bitmap bitmap)
+        {
+            for (int i = 60 + Delay(true); i > 0; i--)
+            {
+                if (bitmap.GetPixel(60 + i, 60) != backgroundColor)
+                {
+                    VirtualKeyCode space = VirtualKeyCode.SPACE;
+                    inputSimulator.Keyboard.KeyDown(space);
+                    Thread.Sleep(Delay(false));
+                    inputSimulator.Keyboard.KeyUp(space);
+
+                    break;
+                }
+            }
+        }
+
+        private void CheckDuckHeight(Bitmap bitmap)
+        {
+            int duckDelay = 5;
+            if (delay > 130)
+            {
+                duckDelay = 15;
+            }
+
+            for (int i = 40 + duckDelay; i > 0; i--)
+            {
+                if (bitmap.GetPixel(50 + i, 26) != backgroundColor)
+                {
+                    if (bitmap.GetPixel(50 + i + 30, 38) != backgroundColor) //akkor madar, ez a farka vege
+                    {
+                        VirtualKeyCode duck = VirtualKeyCode.DOWN;
+                        inputSimulator.Keyboard.KeyDown(duck);
+                        Thread.Sleep(250);
+                        inputSimulator.Keyboard.KeyUp(duck);
+
+                        break;
+                    }
+                }
+            }
+        }
+        
+        private void DelayUpdater()
+        {
+            loopCounter++;
+            if (loopCounter % 90 == 0) delay++;
+        }
+        
+        private int Delay(bool Jump)
+        {
+            if (Jump)
+            {
+                if (delay <= 100)
+                {
+                    return delay;
+                }
+                else return 100;
+            }
+
+            if (!Jump)
+            {
+                if (delay <= 70)
+                {
+                    return 140-delay;
+                }
+                else if (delay > 70 && delay <= 450)
+                {
+                    return 70;
+                } 
+                else if (delay > 450)
+                {
+                    return 90;
+                }
+            }
+
+            return 0;
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            //T.Suspend();
+        }
+
+        private void Updater(Bitmap bitmap)
+        {
+            ss.Image = bitmap;
+            ss.Refresh();
+
+            DelayScreen.Text = delay.ToString();
+            DelayScreen.Refresh();
+        }
+    }
+}
